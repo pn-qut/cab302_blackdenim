eb94115334f6c81dfea2bdc2f61f00d6720cb36e
franwalder <n12078921@qut.edu.au>
2026-09-12 16:06:50 +1000

    Add files for habit tracking functionality

--- src/main/java/com/example/habittracker/model/IHabitDAO.java
+++ src/main/java/com/example/habittracker/model/IHabitDAO.java
@@ -1,6 +1,21 @@
 package com.example.habittracker.model;
 
+import java.util.List;
+
+/**
+ * Interface for the Habit Data Access Object that handles
+ * the operations for the list of default habits within the database.
+ */
 public interface IHabitDAO {
-    // TODO: getAllHabits()
-    // TODO: findHabitById
+    /**
+     * Retrieves all default habits from the database.
+     * @return A list of all default habits in the database.
+     */
+    public List<Habit> getAllHabits();
+
+    /**
+     * Retrieves a habit from the default habits database, searching by id.
+     * @param id The id number of the habit to be retrieved.
+     */
+    public Habit findHabitById(int id);
 }
