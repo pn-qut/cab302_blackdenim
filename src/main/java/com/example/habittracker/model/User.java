30a655c70a4763838356d0d5695268651cc1d7e7
franwalder <n12078921@qut.edu.au>
2026-09-13 19:05:15 +1000

    Combine login backend with user database

--- src/main/java/com/example/habittracker/model/User.java
+++ src/main/java/com/example/habittracker/model/User.java
@@ -1,12 +1,21 @@
 package com.example.habittracker.model;
 
 public class User {
+    private int id;
     private String username;
     private String password;
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     /**
